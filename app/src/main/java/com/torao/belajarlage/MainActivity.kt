05e7fa7ada0597a3e8155f2eea7ac944a89a580f
ari30jackal <ari_rubiks@yahoo.com>
2021-09-08 12:06:26 +0700

    Initial commit

---
+++ app/src/main/java/com/torao/belajarlage/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.torao.belajarlage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.torao.belajarlage.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigationView.setupWithNavController(findNavController(R.id.navHostFragment))
+    }
+}
